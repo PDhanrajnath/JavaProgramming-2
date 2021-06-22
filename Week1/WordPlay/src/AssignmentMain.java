82e5fffbc98bbdc276f3f501698f555acbc4307d
Porika Dhanrajnath <dhanrajnath.porika@zemosolabs.com>
2021-06-23 00:53:47 +0530

    first week

---
+++ Week1/WordPlay/src/AssignmentMain.java
@@ -0,0 +1,10 @@
+public class AssignmentMain {
+    public static void main(String args[]){
+        Assignment obj=new Assignment();
+        obj.testEmphasize();
+        System.out.println();
+        obj.testIsVowel();
+        System.out.println();
+        obj.testReplaceVowels();
+    }
+}
---
+++ Week1/src/MainRunner.java
@@ -0,0 +1,8 @@
+public class MainRunner {
+    public static void main(String args[]){
+        Main obj=new Main();
+        obj.testCaesar();
+        System.out.println();
+        obj.testEncryptTwoKeys();
+    }
+}
